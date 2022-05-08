390f5a360906a2134e76d2799cb3e3d64118906b
Alexandre Schwanke <alexandreschwc@gmail.com>
2022-05-08 00:06:18 -0300

    parcialmente finalizado com o desafio 3

--- desafio-3.js
+++ desafio-3.js
@@ -2,6 +2,7 @@ const n = 10
 const inputArr = [2, 3, 4]
 const arrTeste = [9, 7, 6, 5, 3, 1]
 const arrTeste2 = [2, 5]
+const arrTeste3 = [ 3, 7 ]
 
 function somasDecrescentes (n, inputArr, somasPossiveis) {
     let arr = []
@@ -9,10 +10,13 @@ function somasDecrescentes (n, inputArr, somasPossiveis) {
     let j = inputArr.length -1
     let i = inputArr.length -1
     for (i; i >= 0;) {
-        console.log(`i:${i} - j:${j} - alvo:${alvo}`)
+        // console.log(`i:${i} - j:${j} - alvo:${alvo}`)
         if (inputArr[i] > alvo) {
             while (inputArr[j] > alvo) {
                 j--
+                if (alvo - inputArr[j] < 0) {
+
+                }
                 if (j === -1) {
                     arr = []
                     i--
@@ -36,18 +40,39 @@ function somasDecrescentes (n, inputArr, somasPossiveis) {
 }
 
 function somasAproximadas (n, inputArr, somasPossiveis) {
-    
+    let arr = []
+    let alvo = n
+    let j = inputArr.length -1
+    let i = inputArr.length -1
+
+    for (i; i >= 0;) {
+        if (inputArr[i] > alvo) {
+            console.log(alvo)
+            i--
+        }
+
+        if (inputArr[i] <= alvo || inputArr[j] <= alvo) {
+            arr.push(inputArr[j])
+            alvo -= inputArr[j]
+        }
+    }
 }
 
 function encontraSomas(n, inputArr) {
     const arrOrdenado = inputArr.sort((a, b) => a - b);
 
     let somasPossiveis = []
-    let menoresSomas = []
 
     somasDecrescentes(n, arrOrdenado, somasPossiveis)
+    if (somasPossiveis.length === 0) somasAproximadas(n, arrOrdenado, somasPossiveis)
 
-    console.log(somasPossiveis)
+    let menorLength = somasPossiveis.map(arr => arr.length).reduce((acc, atual) => atual < acc ? atual : acc)
+
+    let menoresSomas = somasPossiveis.filter((arr) => {
+        if (arr.length === menorLength) return arr
+    })
+    
+    console.log(menoresSomas)
 }
 
 encontraSomas(n, arrTeste)
